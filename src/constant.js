d86cbc8386d59232176cacc96bc6fe8d0c2daaeb
Francisco Vena <fvena32@gmail.com>
2018-09-05 12:21:39 +0200

    feat(Constants): Add constant file support

---
+++ src/constant.js
@@ -0,0 +1,10 @@
+export const MODES = {
+  0: 'Auto',
+  1: 'Cold',
+  2: 'Heat',
+};
+
+export const UNITS = {
+  0: 'F',
+  1: 'C',
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,22 @@
 
     .main
       h1 Home Page
+
+      | Unit {{ unit }}
 </template>
 
 <script>
-export default {
+import { UNITS } from '@/constant';
 
+export default {
+  data() {
+    return {
+      unitType: 0,
+    };
+  },
+  computed: {
+    // a computed getter
+    unit() { return `${UNITS[this.unitType]}º`; },
+  },
 };
 </script>
